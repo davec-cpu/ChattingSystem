3c3fbe3578774febc587cd473a82134b61bb4f2a
davec-cpu <hoangdamphi20@gmail.com>
2024-04-26 17:09:59 +0700

    Add project files.

---
+++ ChattingSystem/Models/Participants.cs
@@ -0,0 +1,16 @@
+﻿namespace ChattingSystem.Models
+{
+    public class Participants
+    {
+        public long? Id { get; set; }
+        public long? SiteId { get; set; }
+        public long? UserId { get; set; }
+        public long? ConversationId { get; set; }
+        public string? Title { get; set; }
+        public short? Type { get; set; }
+        public string? Taxonomy { get; set; }
+        public string? Settings { get; set; }
+        public short? Status { get; set; }
+        public long? TotalRecords { get; set; }
+    }
+}
